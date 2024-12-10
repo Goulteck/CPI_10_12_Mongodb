f7d38f9d12800c674a687d7e4d641ca81f3c3689
Goulven Guenan <goulvenguenan@gmail.com>
2024-12-10 12:01:18 +0100

    Update app.js

--- tp_article_api/app.js
+++ tp_article_api/app.js
@@ -18,9 +18,11 @@ mongoose.connection.on('error', () => {
 
 // Se connecter sur mongodb (async)
 // Ca prend x temps à s'executer
-mongoose.connect("mongodb://localhost:27017/db_demo");
+mongoose.connect("mongodb://127.0.0.1:27017/db_demo");
 
 // Todo : Creer le modèle Article
+
+const Article = mongoose.model('Article', {name : String}, 'Articles');
 
 // ================================================
 // Instancier un serveur et autoriser envokie json
@@ -34,8 +36,48 @@ app.use(express.json());
 // ================================================
 // Les routes (url/point d'entrée)
 // ================================================
-app.get('/todo', async (request, response) => {
-    return response.json({ message : "Ceci est juste un exemple"}); 
+
+// une route / un point d'entrée
+app.get('/articles', async (request, response) => {
+    //recuperer toyt les produits dans mongo
+    // Attention asynchrome
+    const articles = await Article.find();
+
+   // RG-001 : Si la liste est vide retourner un code 701
+    if (articles.length == 0){
+        return response.json({ code : "701"});
+    }
+
+   // RG-002 : Sinon la liste des produits
+    return response.json(articles);
+
+});
+
+// URL  GET BY ID
+app.get('/article/:id', async (request, response) => {
+    /// Récupérer le param de l'url
+   const idParam = request.params.id;
+
+   // Récupérer dans la base, le produit avec l'id saisie
+   const foundArticle = await Article.findOne({'_id' : idParam});
+
+    // RG-003 : Si l'id n'existe pas en base code 705
+   if(!foundArticle){
+    return response.json({ code : "705"});
+   }
+
+   // RG-004 : Sinon on retourne le produit trouvé
+
+   return response.json(foundArticle);
+ 
+
+});
+app.post('/save-article', async (request, response) => {
+    return response.json({ message : "Va Créer ou modifiée un article"}); 
+
+});
+app.delete('/article/:id', async (request, response) => {
+    return response.json({ message : "Supprimera l'article de l'id choisie"}); 
 });
 
 // ================================================
